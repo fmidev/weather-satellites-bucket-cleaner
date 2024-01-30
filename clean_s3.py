223c1223f2cea4afad40d866932b734237c031d6
Panu Lahtinen <panu.lahtinen@fmi.fi>
2024-01-30 13:37:27 +0200

    Fix Python styling

--- clean_s3.py
+++ clean_s3.py
@@ -37,5 +37,6 @@ def main():
     elapsed = time.time() - tic
     print(dt.datetime.now(dt.timezone.utc), f"- {num} objects deleted in {elapsed:.1f} seconds")
 
+
 if __name__ == "__main__":
     main()
