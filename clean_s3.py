8347ced4346b5ef7f68521c3c5ae9731c36d1a63
Panu Lahtinen <panu.lahtinen@fmi.fi>
2024-11-04 14:09:18 +0200

    Refactor code

--- clean_s3.py
+++ clean_s3.py
@@ -19,23 +19,38 @@ def main():
     s3 = S3FileSystem()
 
     tic = time.time()
-    num = 0
     for prefix, sub_prefixes, objects in s3.walk(base_uri):
-        for obj in objects:
-            uri = os.path.join(prefix, obj)
-            stat = s3.stat(uri)
-            if stat['LastModified'] < max_age:
-                age = (now - stat['LastModified']).total_seconds() / 3600.
-                age_unit = "hours"
-                if age > 24:
-                    age /= 24.
-                    age_unit = "days"
-                print(f"Delete {uri} - age is {age:.1f} {age_unit}")
-                s3.delete(uri)
-                num += 1
+        num = _check_age_and_delete_objects(s3, prefix, objects, max_age)
 
     elapsed = time.time() - tic
     print(dt.datetime.now(dt.timezone.utc), f"- {num} objects deleted in {elapsed:.1f} seconds")
+
+
+def _check_age_and_delete_objects(s3, prefix, objects, max_age):
+    num = 0
+    for obj in objects:
+        uri = os.path.join(prefix, obj)
+        stat = s3.stat(uri)
+        if stat['LastModified'] < max_age:
+            age = _get_age(stat['LastModified'])
+            _delete_object(s3, uri, age)
+            num += 1
+    return num
+
+
+def _get_age(modified):
+    now = dt.datetime.now(dt.timezone.utc)
+    age = (now - modified).total_seconds() / 3600.
+    age_unit = "hours"
+    if age > 24:
+        age /= 24.
+        age_unit = "days"
+    return f"age is {age:.1f} {age_unit}"
+
+
+def _delete_object(s3, uri, age):
+    print(f"Delete {uri} - {age}")
+    s3.delete(uri)
 
 
 if __name__ == "__main__":
